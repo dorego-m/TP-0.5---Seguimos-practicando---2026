aa51eac5c573fe5efa46a3d753b9f1744c2aa0c9
MANUEL DO REGO BRICCHETTO <50155748@ort>
2026-03-18 09:01:16 -0300

    clases y constructores

---
+++ Alumno.cs
@@ -0,0 +1,28 @@
+namespace TP0._5;
+
+class Alumno 
+{
+
+ // ATRIBUTOS PRIVADOS
+ private string nombre;
+ private int dni;
+ private double cantidadFaltas;
+
+// --- CONSTRUCTOR ---
+public Alumno(string nombre, int dni)
+{
+    this.nombre = nombre;
+    this.dni = dni;
+    this.cantidadFaltas = 0.0;
+}
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Curso.cs
@@ -0,0 +1,25 @@
+namespace TP0._5;
+
+class Curso 
+{
+
+ // ATRIBUTOS PRIVADOS
+ private string nombre;
+ private List<Alumno> Alumnos;
+
+public Curso(string nombre)
+{
+    this.nombre = nombre;
+    this.Alumnos = new List<Alumno>();
+}
+
+
+
+
+
+
+
+
+
+
+}
